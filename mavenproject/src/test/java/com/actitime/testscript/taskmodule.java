9db7176532ff9d09b4f3c2aa97dfb0dcf1cff5e9
vikas <vikas@DESKTOP-J5A2K0T>
2023-10-17 13:41:15 +0530

    second commit

---
+++ mavenproject/src/test/java/com/actitime/testscript/taskmodule.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class taskmodule {
+	@Test
+	public void createmodule(){
+		Reporter.log("hello",true);
+	}
+
+}
