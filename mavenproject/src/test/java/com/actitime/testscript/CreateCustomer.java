0e4c049e3de13612e13bac4b8024a44de98db75d
vikas <vikas@DESKTOP-J5A2K0T>
2023-10-17 13:28:25 +0530

    new work

---
+++ mavenproject/src/test/java/com/actitime/testscript/CreateCustomer.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+	@Test
+	public void createcustomer()
+	{
+		Reporter.log("hi",true);
+	}
+}
+	
